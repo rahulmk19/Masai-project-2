573c459839aa84132316a3a54c16592dba9cbc30
deepak chourasiya <deepak1812002@gmail.com>
2022-09-13 22:28:57 +0530

    correct login logic

--- login.js
+++ login.js
@@ -1,2 +1,11 @@
 let reg_email="rahul@gmail.com";
 let reg_pass="abcd123";
+
+let enter_email = "rahul@gmail.com";
+let enter_pass = "abcd123";
+
+if(reg_email==enter_email && reg_pass==enter_pass){
+    console.log('Login successfully')
+}else{
+    console.log('failed to login ')
+}
